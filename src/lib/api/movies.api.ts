9b7ce6ca3b6aff1ad3dc05bd3f0ce403b7e3414b
TooniOlaniyan <asalamlatif@gmail.com>
2024-06-29 14:32:59 +0100

    chnages: added new url to the 404 image url in the mock json

--- src/lib/api/movies.api.ts
+++ src/lib/api/movies.api.ts
@@ -2,9 +2,10 @@ import { CavistaAPI } from "./axios";
 
 export const fetchSeries = async () => {
   const { data } = await CavistaAPI.get(
-    "/entries?_limit=21&programType=series&releaseYear_gte=2010"
-  );
-   const alphaNumericData = data.sort((a: any, b: any) =>
+    "/entries?_limit=21&programType=series"
+  )
+  const filteredData = data.filter((item: any) => item.releaseYear >= 2010);
+   const alphaNumericData = filteredData.sort((a: any, b: any) =>
      a.title.localeCompare(b.title)
    );
    return alphaNumericData;
@@ -12,10 +13,11 @@ export const fetchSeries = async () => {
 
 };
 export const fetchMovies = async () => {
-  const { data } = await CavistaAPI.get("/entries?_limit=21&programType=movie&releaseYear_gte=2010");
+  const { data } = await CavistaAPI.get("/entries?_limit=21&programType=movie");
 
-  const alphaNumericData = data.sort((a: any, b: any) =>
-    a.title.localeCompare(b.title)
-  );
-  return alphaNumericData;
+ const filteredData = data.filter((item: any) => item.releaseYear >= 2010);
+ const alphaNumericData = filteredData.sort((a: any, b: any) =>
+   a.title.localeCompare(b.title)
+ );
+ return alphaNumericData;
 };
