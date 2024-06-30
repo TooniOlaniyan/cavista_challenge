5a9bf33b1a26bdbf2272d30ab2d78b73c5fa32b0
TooniOlaniyan <asalamlatif@gmail.com>
2024-06-30 18:09:47 +0100

    changes

--- src/lib/api/axios.ts
+++ src/lib/api/axios.ts
@@ -4,7 +4,7 @@ export const BASE_URL = "https://json-server-echb.onrender.com";
 
 
 // export const BASE_URL = 'http://localhost:3000'
-// fall back to localhost when base_url hosted server stops {to start local json server, run npx json-server sample.json}
+// want to view locally on localhost when base_url hosted server stops {to start local json server, run npx json-server sample.json}
 
 export const CavistaAPI = axios.create({
   baseURL: BASE_URL,
